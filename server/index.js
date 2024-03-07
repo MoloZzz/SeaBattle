ebc0b830aa4f8b07d3f804650402f2b4fd1985eb
MoloZzz <mega_oleksii@ukr.net>
2024-03-07 20:36:47 +0200

    ~SOME FIXES

--- server/index.js
+++ server/index.js
@@ -1,21 +1,29 @@
+const fs = require('fs');
 const express = require('express');
-const path = require('path');  
+const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-const port = 7777;
+const port = 7777; 
+
+server.listen(port, () => {
+  console.log(`Сервер запущено на порті ${port}`);
+  console.log(`Server is running at http://localhost:${port}/`);
+});
+
 
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/pages/firstJoin.html'));
+  const filePath = path.join(__dirname, '../client/pages/firstJoin.html');
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+  } else {
+    res.status(404).send('File not found');
+  }
 });
-  
-app.listen(port, () => {
-    console.log(`Сервер прослуховує порт ${port}`);
-    console.log(`Server is running at http://localhost:${port}/`);
-});
+
 
 const users = [];
 const connections = [];
@@ -23,12 +31,11 @@ const connections = [];
 
 io.sockets.on('connection',function(socket){
   console.log('Connected');
-
   connections.push(socket);
 
   socket.on('disconnect', function(){
     connections.slice(connections.indexOf(socket), 1);
     console.log('disconnected');
-
   });
 });
+
